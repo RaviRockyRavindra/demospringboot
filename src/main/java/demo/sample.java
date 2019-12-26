b8bded351d258c5600d9cce96ca6b1e8d357f11e
RaviRockyRavindra <raviforyou1418@gmail.com>
2019-12-26 11:06:00 +0530

    send all files to local git by ravindra

---
+++ src/main/java/demo/sample.java
@@ -0,0 +1,24 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(sample.class, args);
+	}
+	
+	@RequestMapping(value="/rocky")
+	public String run() {
+		
+		return "hello ravindra welcome to team rc creation";
+	}
+	
+
+}
